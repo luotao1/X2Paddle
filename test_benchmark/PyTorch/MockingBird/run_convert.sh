e9a1f6d9913b32da4f80dd18648c3af991385b7b
Tao Luo <luotao02@baidu.com>
2024-12-12 16:30:16 +0000

    revert

--- test_benchmark/PyTorch/MockingBird/run_convert.sh
+++ test_benchmark/PyTorch/MockingBird/run_convert.sh
@@ -1,3 +1,4 @@
+python -m pip install librosa==0.10.2 scikit-learn matplotlib unidecode inflect pypinyin
 # run convert
 python convert_trace.py
 # run infer
--- test_benchmark/PyTorch/mt5_small/run_convert.sh
+++ test_benchmark/PyTorch/mt5_small/run_convert.sh
@@ -1,3 +1,4 @@
+pip install sentencepiece
 # 进行转换
 python convert.py
 # 运行推理程序
